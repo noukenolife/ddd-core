d50be808f74d18ab5fce3c4bdcf020324fce3d38
j-yamamoto <j-yamamoto@onu.edu>
2018-05-03 14:22:31 +0900

    Deleted Future,IORepository

--- src/main/scala/noukenolife/ddd/domain/api/lifecycle/io/Repository.scala
+++
@@ -1,7 +0,0 @@
-package noukenolife.ddd.domain.api.lifecycle.io
-
-import cats.effect.IO
-import noukenolife.ddd.domain.api.lifecycle.{Repository => BaseRepository}
-import noukenolife.ddd.domain.api.model.{Entity, Id}
-
-trait Repository[I <: Id[_], E <: Entity[I]] extends BaseRepository[I, E, IO]
--- src/test/scala/noukenolife/ddd/domain/api/lifecycle/future/FakeRepository.scala
+++ src/test/scala/noukenolife/ddd/domain/api/lifecycle/future/FakeRepository.scala
@@ -1,12 +1,12 @@
 package noukenolife.ddd.domain.api.lifecycle.future
 
-import noukenolife.ddd.domain.api.lifecycle.{EntityNotFoundException, IOContext}
+import noukenolife.ddd.domain.api.lifecycle.{EntityNotFoundException, IOContext, Repository}
 import noukenolife.ddd.domain.api.model.{FakeEntity, FakeId}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class FakeRepository(var entityMap: Map[FakeId, FakeEntity] = Map.empty)
-  (implicit val ec: ExecutionContext) extends Repository[FakeId, FakeEntity] {
+  (implicit val ec: ExecutionContext) extends Repository[FakeId, FakeEntity, Future] {
 
   override def resolve(id: FakeId)(implicit ctx: IOContext): Future[FakeEntity] = Future {
     entityMap.getOrElse(id, throw EntityNotFoundException())
